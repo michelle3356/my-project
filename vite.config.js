e4ea62cce29c20ba6fd4c30978e56cdb8efe9551
LL <lululiphoenix@gmail.com>
2025-06-19 23:17:45 +0800

    config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
 export default defineConfig({
-  base: '/project/', 
+  base: '/my-project/', 
   plugins: [vue()],
   resolve: {
     alias: {
